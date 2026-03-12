561489a4af30271b0320896d01ae8590a9596877
aj20l <156533724+aj20l@users.noreply.github.com>
2026-03-12 08:16:03 -0400

    added csv reading and writing to playlist

--- src/main/java/org/example/Playlist.java
+++ src/main/java/org/example/Playlist.java
@@ -1,13 +1,16 @@
 package org.example;
 
+import java.io.*;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.io.File;
 
 public class Playlist {
     private String name;
     private List<Song> songs;
-
+    private String csvHeader= "Name,Genre,Artist,Lyrics,FilePath";
+    public static final String COMMA_DELIMITER = ",";//"\t";
     public Playlist(String name) {
         this.name = name;
         this.songs = new ArrayList<>();
@@ -37,12 +40,62 @@ public class Playlist {
     public String toString() {
         return name;
     }
-    public void WriteCSV(){} //converts song list to csv so each song is an entry
-    //writes to csv
+    public void writeCSV(){
+        String csvpath="src/main/resources/data/";
+        String fileName= csvpath+name+".csv";
+         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+             writer.write(csvHeader+"\n");
+            for (Song s: songs) {
+                writer.write(s.toCSVString());
+                writer.newLine(); 
+                System.out.println("Successfully wrote to the file.");
 
-    public void readCSV(File csv){
-        //reads playlist csv into list
-
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred: " + e.getMessage());
+            e.printStackTrace();
+        }
 
     }
+        //writes to csv
+
+    public void readCSV(File csv){
+        //reads  csv into list
+        int cursong=0;
+        try (BufferedReader br = new BufferedReader(new FileReader(csv.getName()))) {
+            String line;
+
+            String header=br.readLine();//header check here
+
+            while ((line = br.readLine()) != null) {
+               // System.out.println(csvHeader+"oe");
+              //System.out.println("式"+line);
+                String[] values = line.split(COMMA_DELIMITER);
+
+
+                songs.add(cursong,new Song(values[0],values[4]));
+                cursong++;
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    //songs.remove(0);
+    }
+    public static void main(String[] args) {
+    File csv = new File("src/main/resources/PlaceHolder Name Songs/test.csv");
+        System.out.println(System.getProperty("user.dir"));
+//find out how to change directory
+    Playlist test= new Playlist("PLaylistTest");
+
+
+    test.readCSV(csv);
+         System.out.println(test.songs.toString());
+         test.writeCSV();
+    }
+
+
 }
+
